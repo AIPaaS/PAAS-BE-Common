66dda9e6c0480cc06d67a1c8bbb4d9936944e42f
wanwb <wanwb@asiainfo.com>
2016-03-08 17:12:30 +0800

    添加系统属性池功能

---
+++ src/main/java/com/aic/paas/comm/util/PropertiesPool.java
@@ -0,0 +1,121 @@
+package com.aic.paas.comm.util;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import com.binary.core.io.Resource;
+import com.binary.core.io.ResourceResolver;
+import com.binary.core.util.BinaryUtils;
+import com.binary.core.util.Properties;
+
+public class PropertiesPool implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	private final Map<String, String> properties = new HashMap<String, String>();
+	
+	
+	private String[] locations;
+
+
+	public String[] getLocations() {
+		return locations;
+	}
+	public void setLocation(String location) {
+		BinaryUtils.checkEmpty(location, "location");
+		setLocations(new String[]{location});
+	}
+	public void setLocations(String[] locations) {
+		BinaryUtils.checkEmpty(locations, "locations");
+		this.locations = locations;
+		loadResource();
+	}
+	
+	
+	
+	private void loadResource() {
+		this.properties.clear();
+		for(int i=0; i<locations.length; i++) {
+			Resource res = ResourceResolver.getResource(locations[i]);
+			Properties pro = new Properties(res);
+			this.properties.putAll(pro);
+		}
+	}
+	
+	
+	
+	/**
+	 * 判断键是否存在
+	 * @param key
+	 * @return
+	 */
+	public boolean containsKey(String key) {
+		return properties.containsKey(key);
+	}
+	
+	
+	/**
+	 * 获取值
+	 * @param key
+	 * @return
+	 */
+	public String get(String key) {
+		return properties.get(key);
+	}
+	
+	
+	
+	/**
+	 * 设置值
+	 * @param key
+	 * @param value
+	 */
+	public void set(String key, String value) {
+		this.properties.put(key, value);
+	}
+	
+	
+	
+	
+	/**
+	 * 删除键
+	 * @param key
+	 */
+	public String remove(String key) {
+		return properties.remove(key);
+	}
+	
+	
+	
+	/**
+	 * 清空键值池
+	 */
+	public void clear() {
+		properties.clear();
+	}
+	
+	
+	/**
+	 * 获取所有键
+	 */
+	public Set<String> keySet() {
+		return properties.keySet();
+	}
+	
+	
+	
+	/**
+	 * 获取键值集合
+	 * @return
+	 */
+	public Set<Entry<String, String>> entrySet() {
+		return properties.entrySet();
+	}
+	
+	
+
+}
